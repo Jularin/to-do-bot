2113e073c913c33a386832939b700918e5c6876f
Murat Kamaletdinov <kamaletdinov2425@icloud.com>
2021-02-02 15:06:39 +0300

    add tasks table

--- telegram/database/models.py
+++ telegram/database/models.py
@@ -1,16 +1,31 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
 
 class User(Base):
+    """Table for users"""
     __tablename__ = "users"
-    id = Column(Integer().with_variant(Integer, 'sqlite'), primary_key=True)  # increment primary key
-    user_id = Column(String, nullable=False)
-    username = Column(String)
+    id = Column(Integer().with_variant(Integer, 'sqlite'), primary_key=True, nullable=False)  # increment primary key
+    user_id = Column(String, nullable=False, unique=True)
+    username = Column(String, nullable=True)
+    tasks = relationship("UserTasks", )
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer().with_variant(Integer, 'sqlite'), primary_key=True, nullable=False)  # increment primary key
+    user_id = Column(String, ForeignKey('users.user_id'))
+    user = relationship("User")
+    task_name = Column(String, nullable=False)
+    task_description = Column(Text)
+    group = Column(Text)
+    deadline = Column(DateTime)
+    status = Column(Boolean)  # True - active task, False inactive
+
 
 
 if __name__ == '__main__':
