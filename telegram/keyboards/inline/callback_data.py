50c70d3de8c46c94b53418b5d6951bd8bed67345
Ilnar Gomelyanov <hibushland@gmail.com>
2021-02-03 15:39:46 +0300

    wrote docstrings

--- telegram/keyboards/inline/callback_data.py
+++ telegram/keyboards/inline/callback_data.py
@@ -1,13 +1,17 @@
 from aiogram.utils.callback_data import CallbackData
 
-menu_cd = CallbackData('show_menu', 'level', 'category', 'action', 'subaction', 'delete')
+menu_cd = CallbackData('show_menu', 'level', 'category',
+                       'action', 'subaction', 'delete')
 
 
 def make_callback_data(level, category='0', action='0', subaction='0', delete='0'):
+    
+    '''Creating valid callbak data'''
+
     return menu_cd.new(
         level=level,
         category=category,
-        action=action, 
-        subaction=subaction, 
+        action=action,
+        subaction=subaction,
         delete=delete
     )
--- telegram/keyboards/inline/menu_keybord.py
+++ telegram/keyboards/inline/menu_keybord.py
@@ -2,19 +2,23 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from .callback_data import make_callback_data
 
-#menu_cd,
 
 async def todo_categories_keybord():
-    CURRNET_LEVEL = 0
+
+    '''Creating personal and group buttons'''
+
+    CURRNET_LEVEL = 0 #To know where we are
     markup = InlineKeyboardMarkup()
 
-    personal_callback_data = make_callback_data(level=CURRNET_LEVEL + 1, category='personal')
+    personal_callback_data = make_callback_data(
+        level=CURRNET_LEVEL + 1, category='personal')
     personal_category = InlineKeyboardButton(
         text='Personal todo', callback_data=personal_callback_data
     )
     markup.insert(personal_category)
 
-    group_callback_data = make_callback_data(level=CURRNET_LEVEL + 1, category='group')
+    group_callback_data = make_callback_data(
+        level=CURRNET_LEVEL + 1, category='group')
     group_category = InlineKeyboardButton(
         text='Group todo', callback_data=group_callback_data
     )
